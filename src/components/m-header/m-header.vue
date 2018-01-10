5b0e79eb23f543849176f888b3622bf9157e5fb1
BonLeung <654592397@qq.com>
2018-01-11 01:06:35 +0800

    commit

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <h1 class="text">Chicken Music</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~common/stylus/variable'
+  @import '~common/stylus/mixin'
+
+  .m-header
+    position: relative;
+    height: 44px;
+    text-align: center;
+    color: $color-theme;
+    font-size: 0;
+    .icon
+      display: inline-block;
+      vertical-align: top;
+
+
+</style>
+
+
